c5685d101940e94f421d5990545818c375fb3785
sol1230 <solllll1230@gmail.com>
2023-02-08 15:21:10 +0900

    first commit

---
+++ src/main/java/com/study/study_springboot_security/configurations/SecurityConfiguration.java
@@ -0,0 +1,34 @@
+package com.study.study_springboot_security.configurations;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.web.SecurityFilterChain;
+
+@Configuration
+public class SecurityConfiguration {
+
+  @Bean
+  public SecurityFilterChain filterChain(HttpSecurity httpSecurity)
+    throws Exception {
+    // 권한에 대한 부분 : url & roles : user url & roles
+    httpSecurity
+      .authorizeRequests()
+      // .antMatchers("/").access("hasRole('ROLE_USER') or hasRole('ROLE_ADMIN')")
+      .antMatchers("/")
+      .authenticated() // 로그인 여부만 판단
+      .antMatchers("/admin")
+      .access("hasRole('ROLE_ADMIN')")
+      .anyRequest()
+      .permitAll(); // 설정한 url외 모두 접근 가능
+
+    // 로그인에 대한 부분
+    httpSecurity
+      .formLogin()
+      .loginPage("/loginFrom")
+      .loginProcessingUrl("/loginFrom")
+      .defaultSuccessUrl("/");
+
+    return httpSecurity.build();
+  }
+}
---
+++ src/main/java/com/study/study_springboot_security/controller/MainController.java
@@ -0,0 +1,16 @@
+package com.study.study_springboot_security.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.servlet.ModelAndView;
+
+@Controller
+public class MainController {
+
+  @GetMapping({ "/" }) //일반, 관리자 접속 가능
+  public ModelAndView main(ModelAndView modelAndView) {
+    String viewName = "/WEB-INF/views/main.jsp";
+    modelAndView.setViewName(viewName);
+    return modelAndView;
+  }
+}
